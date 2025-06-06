25820742adbe7bc12366150293d142f6f522db6f
AbhishaValluru <abhishavalluru2020@gmail.com>
2025-06-06 15:49:02 +0530

    Add files via upload

---
+++ nimgame.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] argd)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%4==0)
+        {
+            System.out.println("lose");
+        }
+        else{
+            System.out.println("win");
+        }
+    }
+}
+        
