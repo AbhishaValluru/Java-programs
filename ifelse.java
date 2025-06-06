b5b60c78ca07169184f37a8c2137acde8624e7d4
AbhishaValluru <abhishavalluru2020@gmail.com>
2025-06-06 15:34:42 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] argd)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>0)
+        {
+            System.out.println("positive");
+            }
+            else{
+                System.out.println("negative");
+            }
+        }
+}
