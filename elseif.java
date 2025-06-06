4b33eed38d74632faca5445b43eee72ccff76a4d
AbhishaValluru <abhishavalluru2020@gmail.com>
2025-06-06 15:41:09 +0530

    Add files via upload

---
+++ elseif.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] argd)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>0)
+        {
+            System.out.println("positive");
+            }
+            else if(a>0)
+            {
+                System.out.println("negative");
+            }
+            else
+            {
+                System.out.println("zero");
+            }
+        }
+}
